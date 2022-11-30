79387b4e0adffb9a25be2ab9e5b6bb71f53f8713
kawsarjama <107822116+kawsarjama@users.noreply.github.com>
2022-11-29 23:05:26 -0800

    commited

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == 101 or i == 113:
+    if chr(i) == 'e' or chr(i) == 'q':
         continue
-    print("{:c}".format(i), end='')
+    print("{}".format(chr(i)), end="")
