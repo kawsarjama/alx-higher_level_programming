eb3046ce34403ce615d5e37c7ec22159487fdd93
kawsarjama <107822116+kawsarjama@users.noreply.github.com>
2022-12-01 10:33:18 -0800

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-
 if __name__ == "__main__":
-    arg_num = len(argv) - 1
-    if arg_num == 0:
-        print("{}".format(arg_num))
-    else:
-        result = []
-        for i in range(1, arg_num + 1):
-            result.append(int(argv[i]))
-        print("{}".format(sum(result)))
+    import sys
+    size = len(sys.argv)
+    sum = 0
+    for i in range(1, size):
+        sum += int(sys.argv[i])
+    print(sum)
