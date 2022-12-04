a9f8ad901c1b0847760dca151ec90e3ea2e7a63d
kawsarjama <107822116+kawsarjama@users.noreply.github.com>
2022-12-03 17:50:12 -0800

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def replace_in_list(my_list, idx, element):
     n = len(my_list)
     if idx < 0:
@@ -7,5 +6,5 @@ def replace_in_list(my_list, idx, element):
     elif idx > n - 1:
         return my_list
     else:
-        my_list[idx] =  element
+        my_list[idx] = element
         return my_list
